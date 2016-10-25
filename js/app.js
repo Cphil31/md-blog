bd8bfdbcb77610d22cddfbbad4afeb7775bf6c53
cphil31 <cecile_philippe@live.fr>
2016-10-25 16:24:33 +0200

    étape 1 faite

--- js/app.js
+++ js/app.js
@@ -1,40 +1,71 @@
 
-  "use strict";
-    var app = {
-        init:function(){
+"use strict";
+var app = {
+	
+		init:function(){
             // let's go
             this.listeners();
         },
         listeners:function(){
-            $("#recup").on("click",this.getmarkdown.bind(this));
+        	$("#recup").on("click",this.getmarkdown.bind(this));
+        	$("#recup2").on("click",this.getJson.bind(this));
         },
 
         getmarkdown:function(){
-            $.ajax("http://192.168.1.21:1337/alice.md")
+            $.ajax("http://192.168.1.40:1337/alice.md")
             .done(this.ajaxDone)
             .fail(this.ajaxFail)
             .always(this.ajaxAlways);
         },
         ajaxDone:function(response){
-        	
-	      var converter = new showdown.Converter();
-	      var html = converter.makeHtml(response);
-	      $("#md").html(html);
-	      
-
+            var converter = new showdown.Converter();
+            var html = converter.makeHtml(response);
+            
+            $("#md").html(html);
+            console.log(html);
+            // $("#md").html(response.menu[1].title);
         },
         ajaxFail:function() {
             alert("fail !");
         },
 
         ajaxAlways : function() {
-        
-        },        
-    	};
 
-    	
+        }, 
+
+       getJson:function(){
+            $.ajax("http://192.168.1.40:1337/menu.json")
+            .done(this.jsonDone)
+            .fail(this.jsonFail)
+            .always(this.jsonAlways);
+            
+        },    
+
+        jsonDone:function(){
+          $.get("http://192.168.1.40:1337/menu.json",function(foo){
+            for (var i = 0; i < foo.menu.length; i++) {
+                console.log(foo.menu[i].title);
+            $("#md").append(<a href=> foo.menu[i].title   </a>);
+           
 
 
-    // $(doctransformez le en HTML grace à showdownument).ready(function(){
-        app.init();
+
+            }
+
+
+          })              
+        },
+
+        jsonFail:function() {
+            alert("fail !");
+        },
+
+        jsonAlways : function() {
+            // alert("always !");
+        },   
+    };
+
     
+    // $(doctransformez le en HTML grace à showdownument).ready(function(){
+    	app.init();
+
