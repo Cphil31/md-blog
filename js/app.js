164a03ac9eebb5ada11258f65f701d98516760d4
cphil31 <cecile_philippe@live.fr>
2016-10-25 11:20:52 +0200

    showdown ok

--- js/app.js
+++ js/app.js
@@ -1,13 +1,40 @@
-(function(){
-	"use strict";
-	var app = {
-		init:function(){
-			// let's go
-		},
-	};
+
+  "use strict";
+    var app = {
+        init:function(){
+            // let's go
+            this.listeners();
+        },
+        listeners:function(){
+            $("#recup").on("click",this.getmarkdown.bind(this));
+        },
+
+        getmarkdown:function(){
+            $.ajax("http://192.168.1.21:1337/alice.md")
+            .done(this.ajaxDone)
+            .fail(this.ajaxFail)
+            .always(this.ajaxAlways);
+        },
+        ajaxDone:function(response){
+        	
+	      var converter = new showdown.Converter();
+	      var html = converter.makeHtml(response);
+	      $("#md").html(html);
+	      
+
+        },
+        ajaxFail:function() {
+            alert("fail !");
+        },
+
+        ajaxAlways : function() {
+        
+        },        
+    	};
+
+    	
 
 
-	$(document).ready(function(){
-		app.init();
-	});
-})();
+    // $(doctransformez le en HTML grace à showdownument).ready(function(){
+        app.init();
+    
