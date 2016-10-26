7b03de9dce38158507c842b53cfd47c8ba2b7d95
cphil31 <cecile_philippe@live.fr>
2016-10-26 21:54:57 +0200

    mise a jour

--- js/app.js
+++ js/app.js
@@ -2,14 +2,15 @@
 "use strict";
 var app = {
 	
-  init:function(){
+        init:function(){
             // this.getmarkdown();
-            // this.getJson();
+            this.getJson();
             this.listeners();
         },
+        
         listeners:function(){
-        	$("#recup").on("click",this.getmarkdown.bind(this));
-        	$("#recup2").on("click",this.getJson.bind(this));
+        	$("#md").on("click",this.getmarkdown.bind(this));
+        	// $("#md").on("click",this.getmarkdown.bind(this));
         },
 
         getmarkdown:function(){
@@ -22,10 +23,11 @@ var app = {
             var converter = new showdown.Converter();
             var html = converter.makeHtml(response);
             
-            $("#md").html(html);
-            console.log(html);
+            $("#md").append(html);
+            // console.log(html);
             // $("#md").html(response.menu[1].title);
         },
+
         ajaxFail:function() {
             alert("fail !");
         },
@@ -42,26 +44,25 @@ var app = {
             
         },    
 
-        jsonDone:function(){
-          $.get("http://192.168.1.40:1337/menu.json",function(foo){
+        jsonDone:function(foo){
+          
             for (var i = 0; i < foo.menu.length; i++) {
                 console.log(foo.menu[i]);
-                $("#md").html("<li>"+"<a href=>"+foo.menu[i].title+"</li>");
+                $("#md").prepend('<li><a href="#">'+foo.menu[i].title+'</a></li>');
             }
+              
+          },
 
-        })              
-      },
-
-      jsonFail:function() {
+        jsonFail:function() {
         alert("fail !");
-    },
+        },
 
-    jsonAlways : function() {
-            // alert("always !");
-        },   
-    };
+        jsonAlways : function() {
+                // alert("always !");
+            },   
+        };
 
     
-    // $(doctransformez le en HTML grace à showdownument).ready(function(){
+        // $(doctransformez le en HTML grace à showdownument).ready(function(){
     	app.init();
 
